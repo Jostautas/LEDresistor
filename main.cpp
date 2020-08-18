89f88bd0e448c357f00ec878c38a4cbf5c837d77
Jostautas <jostautas.sakas@gmail.com>
2020-08-18 20:11:22 +0300

    created the project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float led_volt, led_amp, input_volt;
+    cout << "Type in voltage of LED" << endl;
+    cin >> led_volt;
+    cout << "Type in LED current draw (for small LED it's typicaly around o.o2A)" << endl;
+    cin >> led_amp;
+    cout << "Type in the input voltage" << endl;
+    cin >> input_volt;
+    cout << endl << "The value of the resistor should be around " << (input_volt - led_volt) / led_amp << "ohm";
+    return 0;
+}
